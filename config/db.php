65057a6ada0c7da671f0bf66791dd1d7bdf0c019
Pavel <k1osha@derpy.ru>
2020-06-20 23:45:41 +1000

    everything fine

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [               //Подключение к бд
     'class' => '\yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=u0831054_zachem',
     'username' => 'u0831054_zachemchik',
-    'password' =>'zazazachem',
+    'password' =>'zazazachem123',
 
 ];
